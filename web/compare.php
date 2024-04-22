d1b71be76ddf2ef967a0837365d72a073e0d7db5
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2024-04-22 22:36:49 +0300

    plotly is fully functional

--- web/compare.php
+++ web/compare.php
@@ -1,8 +1,11 @@
 <?php
 
 include('inc/env.inc');
-include 'inc/functions.inc';
-include 'inc/tables.inc';
+include('inc/functions.inc');
+include('inc/display_options.inc');
+include('inc/charts.inc');
+include('inc/plotly.inc');
+include('inc/tables.inc');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -80,17 +83,14 @@ if (($model1 != 'unknown') && ($model2 != 'unknown')){
         $heatmap_shown = print_langpair_diffmap($m1_name, $m2_name, $metric, $benchmark, $m1_pkg, $m2_pkg);
     }
     else{
-        $chart_script = $chart == 'diff' ? 'diff-barchart.php' : 'compare-barchart.php';
-        if ( isset( $_COOKIE['PHPSESSID'] ) ) {
-            echo('<img src="'.$chart_script.'?'. SID .'" alt="barchart" /><br/><ul>');
+        if ($renderlib == 'gd'){
+            plot_score_comparison($chart);
         }
         else{
-            $url_param = make_query([]);
-            echo('<img src="'.$chart_script.'?'. $url_param .'" alt="barchart" /><br/><ul>');
+            plot_score_comparison_plotly($chart);
         }
-
         echo('</div><div id="scores">');
-        $langpairs = print_score_table($model1,$model2,$showlang,$benchmark, $metric);
+        $langpairs = print_score_diffs($model1,$model2,$showlang,$benchmark, $metric);
     }
     echo('</div>');
 }
@@ -101,6 +101,7 @@ if (($model1 != 'unknown') && ($model2 != 'unknown')){
 if ($model1 != 'unknown'){
     if ($model2 != 'unknown'){
         echo('<br/><div id="list">');
+        echo('<ul>');
         if (count($langpairs) > 1 && count($langpairs) < 20){
             echo('<ul><li><b>Langpair(s):</b> ');
             ksort($langpairs);
@@ -117,8 +118,10 @@ if ($model1 != 'unknown'){
                 $url_param = make_query(['scoreslang' => 'all']);
                 echo("[<a rel=\"nofollow\" href=\"compare.php?".$url_param."\">all</a>]");
             }
-            echo('</li></ul>');
+            echo('</li>');
         }
+        print_renderlib_link();
+        echo('</ul>');
         echo('<h2>Start with a new model</h2>');
     }
     else{
@@ -193,168 +196,6 @@ echo("</ul>");
 
 
 
-// print a table with all scores and score differences
-
-function print_score_table($model1,$model2,$langpair='all',$benchmark='all', $metric='bleu'){
-    global $table_max_scores;
-    
-    $parts = explode('/',$model1);
-    $pkg1 = array_shift($parts);
-    $name1 = implode('/',$parts);
-    $lines1 = read_scores($langpair, 'all', $metric, $name1, $pkg1);
-    // list($pkg1, $lang1, $name1) = explode('/',$model1);
-    // $lines1 = read_scores($langpair, 'all', $metric, implode('/',[$lang1,$name1]), $pkg1);
-
-    $testsets = array();
-    $langpairs = array();
-    $scores1 = array();
-    foreach($lines1 as $line1) {
-        // echo $line1;
-        $array = explode("\t", $line1);
-        
-        if (! isset($langpairs[$array[0]])) $langpairs[$array[0]] = 0;
-        if (! isset($testsets[$array[1]]))  $testsets[$array[1]] = 0;
-
-        $langpairs[$array[0]]++;
-        $testsets[$array[1]]++;
-        if ($langpair == 'all' || $langpair == $array[0]){
-            if ($benchmark == 'all' || $benchmark == $array[1]){
-                $key = $array[0].'/'.$array[1];
-                // $score = $metric == 'bleu' ? $array[3] : $array[2];
-                $score = $array[2];
-                $scores1[$key] = $score;
-            }
-        }
-    }
-
-    $parts = explode('/',$model2);
-    $pkg2 = array_shift($parts);
-    $name2 = implode('/',$parts);
-    $lines2 = read_scores($langpair, 'all', $metric, $name2, $pkg2);
-    // list($pkg2, $lang2, $name2) = explode('/',$model2);
-    // $lines2 = read_scores($langpair, 'all', $metric, implode('/',[$lang2,$name2]), $pkg2);
-
-
-    $common_langs = array();
-    $common_tests = array();
-    $scores2 = array();
-    foreach($lines2 as $line2) {
-        $array = explode("\t", $line2);
-        if (array_key_exists($array[0],$langpairs)){
-            if (! isset($common_langs[$array[0]])) $common_langs[$array[0]] = 0;
-            $common_langs[$array[0]]++;
-        }
-        if (array_key_exists($array[1],$testsets)){
-            if (! isset($common_tests[$array[1]])) $common_tests[$array[1]] = 0;
-            $common_tests[$array[1]]++;
-        }
-        if ($langpair == 'all' || $langpair == $array[0]){
-            if ($benchmark == 'all' || $benchmark == $array[1]){
-                $key = $array[0].'/'.$array[1];
-                // $score = $metric == 'bleu' ? $array[3] : $array[2];
-                $score = $array[2];
-                $scores2[$key] = $score;
-            }
-        }
-    }
-    
-    $avg_score1 = 0;
-    $avg_score2 = 0;
-    $count_scores1 = 0;
-    $count_scores2 = 0;
-
-    
-    echo('<div id="scores"><div class="query"><table>');
-    if (count($scores1) > $table_max_scores){
-        echo "<p>There are ".count($scores1)." $metric scores for model 1. Show max $table_max_scores!</p>";
-    }
-
-    echo("<tr><th>ID</th><th>Language</th><th>Benchmark ($metric)</th><th>Output</th><th>Model 1</th><th>Model 2</th><th>Diff</th></tr>");
-    $id = 0;
-
-    foreach($scores1 as $key => $score1) {
-        if ($id > $table_max_scores){
-            break;
-        }
-        if (array_key_exists($key,$scores2)){
-            $score2 = $scores2[$key];
-
-            $diff = $score1 - $score2;
-            $diff_pretty = $metric == 'bleu' ? sprintf('%4.1f',$diff) : sprintf('%5.3f',$diff);
-
-            list($lang, $test) = explode('/',$key);
-            $testsets[$test]++;
-            $common_langs[$lang]++;
-
-            $lang_query = array();
-            $test_query = array();
-            
-            if ($langpair == 'all' || $langpair == $lang){
-                if ($benchmark == 'all' || $benchmark == $test){
-                    $avg_score1 += $score1;
-                    $count_scores1++;
-                    $avg_score2 += $score2;
-                    $count_scores2++;
-                    
-                    if (! array_key_exists($lang,$lang_query)){
-                        $query = make_query(['scoreslang' => $lang]);
-                        $lang_query[$lang] = '<a rel="nofollow" href="compare.php?'.$query.'">'.$lang.'</a>';
-                    }
-                    if (! array_key_exists($test,$test_query)){
-                        $query = make_query(['test' => $test]);
-                        $test_query[$test] = '<a rel="nofollow" href="compare.php?'.$query.'">'.$test.'</a>';
-                    }
-
-                    $query = make_query(['test' => $test,'langpair' => $lang, 'start' => 0, 'end' => 9]);
-                    $translink = "<a rel=\"nofollow\" href=\"compare-translations.php?".SID.'&'.$query."\">compare</a>";
-                    
-                    echo('<tr><td>');
-                    echo(implode('</td><td>',[$id, $lang_query[$lang], $test_query[$test], $translink, $score1, $score2, $diff_pretty]));
-                    echo('</td></tr>');
-                    $id++;
-                }
-            }
-        }
-    }
-        
-    if ($count_scores1 > 1){
-        $avg_score1 /= $count_scores1;
-    }
-    if ($count_scores2 > 1){
-        $avg_score2 /= $count_scores2;
-    }
-    $diff = $avg_score1 - $avg_score2;
-    
-    if ($metric == 'bleu'){
-        $avg1 = sprintf('%4.1f',$avg_score1);
-        $avg2 = sprintf('%4.1f',$avg_score2);
-        $diff = sprintf('%4.1f',$diff);
-    }
-    else{
-        $avg1 = sprintf('%5.3f',$avg_score1);
-        $avg2 = sprintf('%5.3f',$avg_score2);
-        $diff = sprintf('%5.3f',$diff);
-    }
-
-    $langlink = '';
-    $testlink = '';
-    if ($langpair != 'all'){
-        if (sizeof($common_langs) > 1){
-            $url_param = make_query(['scoreslang' => 'all']);
-            $langlink = "<a rel=\"nofollow\" href=\"compare.php?".$url_param."\">show all</a>";
-        }
-    }
-    if ($benchmark != 'all'){
-        if (sizeof($testsets) > 1){
-            $url_param = make_query(['test' => 'all']);
-            $testlink = "<a rel=\"nofollow\" href=\"compare.php?".$url_param."\">show all</a>";
-        }
-    }
-    echo("<tr><th></th><th>$langlink</th><th>$testlink</th><th>average</th><th>$avg1</th><th>$avg2</th><th>$diff</th></tr>");
-
-    echo('</table></div></div>');
-    return $common_langs;
-}
 
 include('footer.php');
 
--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
 
 include('inc/env.inc');
-include 'inc/functions.inc';
-include 'inc/display_options.inc';
-include 'inc/charts.inc';
-include 'inc/plotly.inc';
-include 'inc/tables.inc';
+include('inc/functions.inc');
+include('inc/display_options.inc');
+include('inc/charts.inc');
+include('inc/plotly.inc');
+include('inc/tables.inc');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -21,24 +21,9 @@ include 'inc/tables.inc';
 
 
 // get query parameters
-$package     = get_param('pkg', 'opusmt');
-$benchmark   = get_param('test', 'all');
-$metric      = get_param('metric', 'bleu');
-$showlang    = get_param('scoreslang', 'all');
-$model       = get_param('model', 'top');
 $chart       = get_param('chart', 'standard');
 $chartlegend = get_param('legend', 'type');
 $userscores  = get_param('userscores', 'no');
-
-
-list($srclang, $trglang, $langpair) = get_langpair();
-
-if ($showlang != 'all'){
-    if ($showlang != $langpair){
-        set_langpair($showlang);
-        list($srclang, $trglang, $langpair) = get_langpair();
-    }
-}
 
 
 include 'header.php';
@@ -48,36 +33,15 @@ echo('<div id="chart">');
 
 
 
-// Create the link list with different options
-
-$multilingual_model = is_multilingual_model($model);
-$userscores_exists = local_scorefile_exists($langpair, 'all', $metric, 'all', 'contributed', 'user-scores');
-
-$langpair_link = langpair_link();
-$model_selection_links = model_selection_links();
-$compare_link = compare_link();
-$benchmark_link = benchmark_link();
-
-
-// create the list with settings and options
+// Create the link list with different display options
 
 echo("<ul>");
-echo("<li><b>Language pair:</b> $langpair_link</li>");
-echo("<li><b>Models:</b> $model_selection_links $compare_link</li>");
-if ($model != 'all' && $model != 'top'){
-    echo("<li><b>Selected:</b> $model</li>");
-}
-echo("<li><b>Benchmark:</b> $benchmark_link</li>");
-echo("<li><b>Evaluation metric:</b> ");
-print_metric_options($metric);
-echo("</li>");
+print_langpair_link();
+print_model_selection_links();
+print_benchmark_link();
+print_metric_links();
 print_chart_type_links();
 echo("</ul>");
-
-
-$heatmap_shown = false;
-$barchart_script = 'barchart.php';
-
 
 
 // different views that will be available:
@@ -93,77 +57,99 @@ $barchart_script = 'barchart.php';
 // (1) compare top scores OPUS-MT vs external (+ contributed in table)
 
 if ($model == 'top' && $benchmark == 'all'){
-    $barchart_script = $chart == 'diff' ? 'diff-barchart.php' : 'compare-barchart.php';
-    $url_param = make_query(['model1' => 'unknown', 'model2' => 'unknown']);
-    if ( isset( $_COOKIE['PHPSESSID'] ) ) {
-        echo("<img src=\"$barchart_script?". SID .'&'.$url_param."\" alt=\"barchart\" />");
+    set_param('model1', 'unknown');
+    set_param('model2', 'unknown');
+    if ($renderlib == 'gd'){
+        plot_score_comparison($chart);
     }
     else{
-        echo("<img src=\"$barchart_script?$url_param\" alt=\"barchart\" />");
+        plot_score_comparison_plotly($chart);
     }
     echo('<ul>');
     echo('<li>blue = OPUS-MT / Tatoeba-MT models, grey = external models, purple = user-contributed</li>');
+    print_renderlib_link();
     print_contributed_link();
     echo('</ul>');
+    echo '</div><div id="scores" class="query">';
+    if ($chart == "diff"){
+        print_topscore_differences($langpair, $benchmark, $metric, 'no');
+    }
+    else{
+        print_topscore_differences($langpair, $benchmark, $metric, $userscores);
+    }
+    echo('</div>');
 }
 
 
 // (2) top scores for OPUS-MT or external
 
 elseif ($model == 'all' && $benchmark == 'all'){
-    $lines = read_model_scores($langpair, $benchmark, $metric, $model, $package);
-    list($data,$type) = read_barchart_scores($lines, $model, $benchmark, $chartlegend, $showlang);
-    barchart_plotly($data,$type, $metric);
+    // if ($chart == 'scatterplot' || $renderlib == 'gd'){
+    if ($renderlib == 'gd'){
+        plot_scores($chart);
+    }
+    else{
+        plot_topscores_plotly();
+    }
     echo('<ul>');
     echo('<li>orange = OPUS-MT, blue = Tatoeba-MT models, red = HPLT-MT models</li>');
     echo('<li>green = student models, grey = external models, purple = user-contributed</li>');
+    print_renderlib_link();
     print_contributed_link();
     echo('</ul>');
+    echo '</div><div id="scores" class="query">';
+    print_scores($model, $langpair,$benchmark,$package,$metric);
+    echo('</div>');
 }
 
 
 // (3) averaged scores for all models
 
 elseif ($benchmark == 'avg'){
-    if ($chart == 'scatterplot'){
-        $url_param = make_query(['model1' => 'unknown', 'model2' => 'unknown']);
-        if ( isset( $_COOKIE['PHPSESSID'] ) ) {
-            echo("<img src=\"scatterplot.php?". SID .'&'.$url_param."\" alt=\"barchart\" />");
-        }
-        else{
-            echo("<img src=\"scatterplot.php?$url_param\" alt=\"barchart\" />");
-        }
+    // if ($chart == 'scatterplot' || $renderlib == 'gd'){
+    if ($renderlib == 'gd'){
+        plot_scores($chart);
     }
     else{
-        $lines = read_model_scores($langpair, $benchmark, $metric, $model, $package);
-        list($data,$type) = read_barchart_scores($lines, $model, $benchmark, $chartlegend, $showlang);
-        barchart_plotly($data,$type, $metric);
+        plot_benchmark_scores_plotly($chart);
     }
     if ($chartlegend == 'size'){ print_size_legend(); }
     echo('<ul>');
     echo('<li>orange = OPUS-MT, blue = Tatoeba-MT models, red = HPLT-MT models</li>');
     echo('<li>green = student models, grey = external models, purple = user-contributed</li>');
+    print_renderlib_link();
     print_contributed_link();
     echo('</ul>');
+    echo '</div><div id="scores" class="query">';
+    print_scores($model, $langpair,$benchmark,$package,$metric);
+    echo('</div>');
 }
 
 
 // (4) scores for a specific model
 
 elseif ($model != 'top' && $model != 'all' && $model != 'verified' && $model != 'unverified'){
+    $chartlegend = 'type';
     if ($chart == 'heatmap'){
-        $heatmap_shown = print_langpair_heatmap($model, $metric, $benchmark, $package);
+        print_langpair_heatmap($model, $metric, $benchmark, $package);
     }
     else{
-        $lines = read_model_scores($langpair, $benchmark, $metric, $model, $package);
-        list($data,$type) = read_barchart_scores($lines, $model, $benchmark, $chartlegend, $showlang);
-        barchart_plotly($data,$type, $metric);
+        if ($renderlib == 'gd'){
+            plot_scores($chart);
+        }
+        else{
+            plot_model_scores_plotly();
+        }
         if ($chartlegend == 'size'){ print_size_legend(); }
         echo('<ul>');
         echo('<li>orange = OPUS-MT, blue = Tatoeba-MT models, red = HPLT-MT models</li>');
         echo('<li>green = student models, grey = external models, purple = user-contributed</li>');
+        print_renderlib_link();
         print_contributed_link();
         echo('</ul>');
+        echo '</div><div id="scores" class="query">';
+        print_model_scores($model,$showlang,$benchmark,$package,$metric);
+        echo('</div>');
     }
 }
 
@@ -171,26 +157,23 @@ elseif ($model != 'top' && $model != 'all' && $model != 'verified' && $model != 
 // (5) scores for a specific benchmark
 
 elseif ($benchmark != 'avg' && $benchmark != 'all'){
-    if ($chart == 'scatterplot'){
-        $url_param = make_query(['model1' => 'unknown', 'model2' => 'unknown']);
-        if ( isset( $_COOKIE['PHPSESSID'] ) ) {
-            echo("<img src=\"scatterplot.php?". SID .'&'.$url_param."\" alt=\"barchart\" />");
-        }
-        else{
-            echo("<img src=\"scatterplot.php?$url_param\" alt=\"barchart\" />");
-        }
+    // if ($chart == 'scatterplot' || $renderlib == 'gd'){
+    if ($renderlib == 'gd'){
+        plot_scores($chart);
     }
     else{
-        $lines = read_model_scores($langpair, $benchmark, $metric, $model, $package);
-        list($data,$type) = read_barchart_scores($lines, $model, $benchmark, $chartlegend, $showlang);
-        barchart_plotly($data,$type, $metric);
+        plot_benchmark_scores_plotly($chart);
     }
     if ($chartlegend == 'size'){ print_size_legend(); }
     echo('<ul>');
     echo('<li>orange = OPUS-MT, blue = Tatoeba-MT models, red = HPLT-MT models</li>');
     echo('<li>green = student models, grey = external models, purple = user-contributed</li>');
+    print_renderlib_link();
     print_contributed_link();
     echo('</ul>');
+    echo '</div><div id="scores" class="query">';
+    print_scores($model, $langpair,$benchmark,$package,$metric);
+    echo('</div>');
 }
 
 
@@ -198,35 +181,7 @@ elseif ($benchmark != 'avg' && $benchmark != 'all'){
 // different script (compare.php)
 
 
-
-
-
-
-/////////////////////////////////////////////////////
-// score table
-/////////////////////////////////////////////////////
-
-
-if ( ! $heatmap_shown ){
-    echo '</div><div id="scores" class="query">';
-    if ($model == 'top' && $benchmark == 'all'){
-        if ($chart == "diff"){
-            print_topscore_differences($langpair, $benchmark, $metric, 'no');
-        }
-        else{
-            print_topscore_differences($langpair, $benchmark, $metric, $userscores);
-        }
-    }
-    elseif ($model != 'all' && $model != 'top'){
-        print_model_scores($model,$showlang,$benchmark,$package,$metric);
-    }
-    else{
-        print_scores($model, $langpair,$benchmark,$package,$metric);
-    }
-    echo('</div>');
-}
-
-
+// echo("</ul>");
 include('footer.php');
 
 ?>
